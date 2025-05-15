d172ade51db4248e4020d63de0d04008458fa961
alperen daş <117278469+alzimar104@users.noreply.github.com>
2025-05-15 23:05:55 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# Gerekli paketleri kur
 pip install -r requirements.txt
-
-# Ana uygulamayı başlat
-uvicorn main:app --host 0.0.0.0 --port 8000 --reload
+uvicorn main:app --host 0.0.0.0 --port $PORT
